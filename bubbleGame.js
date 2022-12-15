5651bef90aa7d78e4cb4985e6c884ebb722a1a12
cbmarie255 <court.b1993@gmail.com>
2022-12-15 15:09:48 -0600

    Project updates- good luck guys!

---
+++ bubbleGame.js
@@ -0,0 +1,44 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        defaut: 'arcade',
+        arcade: {
+            gravity: {y: 5},
+            debug: false
+        },
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+let player;
+let click;
+let bubbles;
+let score = 0;
+let fallSpeed;
+let cursors;
+
+function preload() {
+    this.load.image('background', 'assets/background.jpeg');
+    this.load.spritesheet('pointer', 'assets/pointing_finger_sprite.jpeg');
+    this.load.audio('backgroundMusic', 'assets/Adventure-320bit.mp3');
+    this.load.spritesheet('bubbles', 'assets/bubble_sprite_sheet.jpeg');
+}
+
+function create() {
+    this.add.image(400, 300, 'background');
+    score = this.add.text(16, 16, 'Your Score Is: 0', {fontSize: '32px', fill: '#000'});
+    player = this.physics.add.sprite(200, 150, 'pointer');
+    player.setCollideWorldBounds(true);
+    bubbles = this.physics.add.sprite(400, 300, 'bubbles');
+    bubbles.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
+    click = this.input.click(Phaser.Input.click)
+}
+
